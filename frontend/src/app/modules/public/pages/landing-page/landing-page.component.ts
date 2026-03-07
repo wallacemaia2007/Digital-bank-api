5397b5dbdbc383d43c71696d5586e6a126dd23d2
Wallace Maia <wallacemaia2007@gmail.com>
2026-03-06 22:04:54 -0300

    feat: add navbar component with navigation items and integrate into landing page

--- frontend/src/app/modules/public/pages/landing-page/landing-page.component.ts
+++ frontend/src/app/modules/public/pages/landing-page/landing-page.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { UserCardComponent } from "../../../shared/components/user-card/user-card.component";
+import { UserCardComponent } from '../../../shared/components/user-card/user-card.component';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [UserCardComponent],
+  imports: [UserCardComponent, NavbarComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
-
+  navItems = [
+    { label: 'Home', link: '/' },
+    { label: 'About', link: '/about' },
+    { label: 'Contact', link: '/contact' },
+  ];
 }
